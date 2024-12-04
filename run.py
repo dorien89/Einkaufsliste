49bd88a14661f7303d851c1c473bb51700b5c7fb
Dorien Druff <dorien.druff@gmx.de>
2024-12-04 21:57:15 +0100

    save current state

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    print("Server startet auf http://127.0.0.1:5000")
+    app.run(port=5000, debug=True)
